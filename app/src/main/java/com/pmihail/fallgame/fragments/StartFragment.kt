9aad5ec16c0c65ffe3f96c8bfda8f54f9f7373de
Mihail P <mihail.pantuyhin@alseda.by>
2020-01-06 13:46:44 +0300

    add details fragment

--- app/src/main/java/com/pmihail/fallgame/fragments/StartFragment.kt
+++ app/src/main/java/com/pmihail/fallgame/fragments/StartFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.pmihail.fallgame.R
+import kotlinx.android.synthetic.main.start_fragment.*
 
 class StartFragment: Fragment() {
 
@@ -16,7 +19,10 @@ class StartFragment: Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.start_fragment, container, false)
-
+        val btnNext = view.findViewById<Button>(R.id.btnNext)
+        btnNext.setOnClickListener {
+            findNavController().navigate(R.id.detailsFragment)
+        }
 
         return view
     }
