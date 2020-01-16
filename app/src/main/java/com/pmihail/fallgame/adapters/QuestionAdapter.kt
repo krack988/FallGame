307149e1724fc88ae76baf232c64b078fbbef8f2
Mihail P <mihail.pantuyhin@alseda.by>
2020-01-16 21:51:22 +0300

    add animation res

--- app/src/main/java/com/pmihail/fallgame/adapters/QuestionAdapter.kt
+++ app/src/main/java/com/pmihail/fallgame/adapters/QuestionAdapter.kt
@@ -1,8 +1,11 @@
 package com.pmihail.fallgame.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
+import com.pmihail.fallgame.R
 import com.pmihail.fallgame.data.AnswerVariant
 
 class QuestionAdapter : RecyclerView.Adapter<QuestionAdapter.ViewHolder>() {
@@ -11,18 +14,23 @@ class QuestionAdapter : RecyclerView.Adapter<QuestionAdapter.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ViewHolder(parent.inflate(R.layout.row_empty_card))
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 5
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun updateItems(list: List<AnswerVariant>) {
 
     }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+        private fun ViewGroup.inflate(@LayoutRes layout: Int, attach: Boolean = false) =
+            LayoutInflater.from(context).inflate(layout, this, attach)
+
 }
